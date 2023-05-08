0181fcb796a1eda401ec80d1c4fdce1090cfac43
Ainan Salaha <ainansalaha77@gmail.com>
2023-05-08 16:06:04 +0530

    ecome-done

---
+++ connection/db.js
@@ -0,0 +1,14 @@
+const mongoose= require('mongoose')
+
+
+const connection= async()=>{
+    try {
+        console.log("Connecting...")
+        await mongoose.connect(process.env.URL)
+        console.log("Connnected to db")
+    } catch (error) {
+        console.log(`db.js->${error}`)
+    }
+}
+
+module.exports= connection
